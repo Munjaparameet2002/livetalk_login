1c0c396353be921820156ef6aa73aef64243e9d2
Admin <meet2002munjapara@gmail.com>
2024-02-02 23:34:36 +0530

    hhh

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:livetalk_login/firebase/login.dart';
-import 'package:livetalk_login/verible.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
@@ -25,7 +19,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: b_col,title: Text("Livetalk",style: TextStyle(color: g_col),)),
+      appBar: AppBar(title: Text("Livetalk")),
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         SizedBox(
           height: 150,
@@ -50,16 +44,13 @@ class _HomeState extends State<Home> {
               onPressed: () {
                 signInWithGoogle();
               },
-              style: ElevatedButton.styleFrom(fixedSize: Size(220, 35)),
+              style: ElevatedButton.styleFrom(fixedSize: Size(175, 35)),
               icon: Image.asset(
                 "images/1.jpg",
                 width: 20,
                 height: 20,
               ),
-              label: Text(
-                "Sign in with Google",
-                style: TextStyle(color: go_col),
-              ),
+              label: Text("Sign in with Google"),
             )
           ],
         ),
@@ -70,12 +61,9 @@ class _HomeState extends State<Home> {
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue.shade900,
-                    fixedSize: Size(220, 35)),
-                icon: Icon(Icons.facebook, color: g_col),
-                label: Text(
-                  "Sign in with Facebook",
-                  style: TextStyle(color: g_col),
-                ))
+                    fixedSize: Size(175, 35)),
+                icon: Icon(Icons.facebook),
+                label: Text("Sign in with Facebook"))
           ],
         ),
         Row(
@@ -84,10 +72,9 @@ class _HomeState extends State<Home> {
             ElevatedButton.icon(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber, fixedSize: Size(220, 35)),
-              icon: Icon(Icons.account_circle_rounded, color: g_col),
-              label:
-                  Text(" Continue as a guest", style: TextStyle(color: g_col)),
+                  backgroundColor: Colors.amber, fixedSize: Size(175, 35)),
+              icon: Icon(Icons.account_circle_rounded),
+              label: Text(" Continue as a guest"),
             )
           ],
         )
